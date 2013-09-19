306755b430050ce97460224af66164f1afa60816
MONY <mony@mony-Latitude-E4300.(none)>
2013-09-19 17:08:26 +0700

    My first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolApp::Application.config.secret_token = '2c27eb16d567ea42d8fb8197124574d70995772950cce0be2d96d9b9ccc1355564044981365efbd535714d60f32863f42653110b6fcfad297feda3c5efbc06b3'
